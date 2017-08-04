e7d770a678517330b7a28d711c3270c608f74146
Andrew Hutchings <andrew.hutchings@gmail.com>
2017-08-03 20:08:14 -0400

    Enable tests

--- server.js
+++ server.js
@@ -1,8 +1,4 @@
-const express = require('express');
-
-const app = express();
-app.disable('x-powered-by');
-app.use(express.static('public'));
+const app = require('./lib/app');
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
