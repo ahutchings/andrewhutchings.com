e8afd2597e4ad69af9a988dc97c7d3cb5af24505
Andrew Hutchings <andrew.hutchings@gmail.com>
2019-07-05 14:41:05 -0400

    Feature/netlify (#7)
    Configure site for netlify

--- server.js
+++
@@ -1,9 +0,0 @@
-const app = require('./lib/app');
-
-const port = process.env.PORT || 3000;
-const server = app.listen(port, () => {
-  const host = server.address().address;
-  const port = server.address().port;
-
-  console.log('andrewhutchings listening at http://%s:%s', host, port);
-});
